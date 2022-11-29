9c732f0467288ab186d5b48d6dfe3df4030a7736
Spencer Stone <spencer.w.stone@gmail.com>
2022-11-29 14:54:27 -0800

    use gray scale BT.601

--- src/img2wav.c
+++ src/img2wav.c
@@ -15,12 +15,22 @@ float map(float src, float src_min, float src_max, float target_min, float targe
     return target_min + ((target_max - target_min) * (src - src_min)) / (src_max - src_min);
 }
 
-/** clamp a range of values to be between [-1.0, 1.0] */
-void clamp(float *src, size_t n) {
-    for (size_t i = 0; i < n; i++) {
-        if (src[i] > 1.0f) src[i] = 1.0f;
-        if (src[i] < -1.0f) src[i] = -1.0f;
-    }
+/** Find absolute maximum value in array */
+float find_max(float *src, size_t n) {
+    float max = 0.0f;
+    for (size_t i = 0; i < n; i++)
+        if (fabs(src[i]) > max)
+            max = src[i];
+
+    return fabs(max);
+}
+
+/** Normalize values in array to be [-1.0, 1.0] */
+void normalize(float *src, size_t n) {
+    float max  = find_max(src, n);
+    float norm = 1.0f / max;
+    for (size_t i = 0; i < n; i++)
+        src[i] *= norm;
 }
 
 /**
@@ -33,9 +43,15 @@ Convert an image into a Width x Height sized array of greyscale values between [
 */
 int *get_pixels(const char *path, int *x, int *y) {
     int n;
-    unsigned char *data = stbi_load(path, x, y, &n, 0);
+    int comp            = 3;
+    unsigned char *data = stbi_load(path, x, y, &n, comp);
     if (!data) {
         fprintf(stderr, "Failed to open file: %s!\n", path);
+        return NULL;
+    }
+
+    if (n != comp) {
+        fprintf(stderr, "Image stride must be 3 (RGB) got %d!\n", n);
         return NULL;
     }
 
@@ -48,21 +64,12 @@ int *get_pixels(const char *path, int *x, int *y) {
     for (int i = 0; i < *y; i++) {
         int col = 0;
         for (int j = 0; j < *x * n; j += n) {
-            /* 
-            take the average of our pixel data based on the stride n
-            we do it this way since we cant know the stride at compile time
-            so just iterate over the stride using an offset 
-            */
-            int off = 0;
-            int avg = 0;
-            while (off < n) {
-                avg += data[i * (*x * n) + j + off];
-                off++;
-            }
-            avg /= n;
-            avg = 255 - avg;
+            const int r = data[i * (*x * n) + j];
+            const int g = data[i * (*x * n) + j + 1];
+            const int b = data[i * (*x * n) + j + 2];
 
-            pixels[i * *x + col] = avg;
+            // convert the image to gray scale using BT.601
+            pixels[i * *x + col] = (r * 0.299 + g * 0.587 + b * 0.114);
             col++;
         }
     }
@@ -77,19 +84,20 @@ Convert pixel data to frequency data for use in generating audio files
 
 @param pixels Single channel pixel data of values between [0, 255]
 @param sample_rate Desired sample rate of the frequencies
-@param duration_s Length in seconds of the transmission time
+@param time_s Length in seconds of the transmission time
 @param width Width of the pixel data
 @param height Height of the pixel data
 @param size Pointer to the number of generated amplitudes
 @return Array of amplitudes corresponding to the frequencies generated
 */
-float *get_freqs(const int *pixels, float sample_rate, float duration_s, int width, int height, int *size) {
-    const float two_pi = M_PI * 2.0;
-    const float fs     = sample_rate;
-    const float time   = duration_s;// length in seconds
-    const int target   = (fs * time) / width;
+float *get_freqs(const int *pixels, float sample_rate, float time_s, int width, int height, int *size) {
+    const float two_pi   = M_PI * 2.0;
+    const float fs       = sample_rate;
+    const int target     = (fs * time_s) / width;
+    const float max_freq = 48000.0;// maximum displayed frequency in the spectrogram
+    const float scale    = max_freq / height;
+    *size                = time_s * fs;
 
-    *size         = time * fs;
     float *result = calloc(*size, sizeof(*result));
     if (!result) {
         fprintf(stderr, "Failed to allocate: %d bytes in img2freq()!\n", *size);
@@ -102,10 +110,12 @@ float *get_freqs(const int *pixels, float sample_rate, float duration_s, int wid
             if (pixels[y * width + x] < 10) continue;// skip nonexistant pixels
 
             const float heat = pixels[y * width + x];
-            const float A    = map(heat, 0.0f, 255.0f, 0.01f, 0.1f);
+            const float A    = map(heat, 0.0f, 255.0f, 0.001f, 1.0f);
             int t            = 0;
             while (t < target) {
-                rp[t] += A * sin(two_pi * (((height - y) * 500.0) / fs) * t);
+                // we do (height - y) to flip the image upside down
+                const float fc = (height - y) * scale;
+                rp[t] += A * sin(two_pi * (fc / fs) * t);
                 t++;
             }
         }
@@ -118,7 +128,7 @@ float *get_freqs(const int *pixels, float sample_rate, float duration_s, int wid
 int main(int argc, char **argv) {
     if (argc < 5) {
         printf("img2wav - Convert an image to the frequency spectrum of an audio file\n"
-               "Usage: img2wav [sampleRate] [duration_s] in.jpg out.wav\n");
+               "Usage: img2wav [sample_rate] [time_s] in.jpg out.wav\n");
 
         return EXIT_SUCCESS;
     }
@@ -130,9 +140,9 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    const float duration_s = atof(argv[2]);
-    if (duration_s == 0.0) {
-        printf("Invalid duration: %f!", duration_s);
+    const float time_s = atof(argv[2]);
+    if (time_s == 0.0) {
+        printf("Invalid transmission time: %f!", time_s);
 
         return EXIT_FAILURE;
     }
@@ -142,12 +152,12 @@ int main(int argc, char **argv) {
     if (!pixels) return EXIT_FAILURE;
 
     int n;
-    float *freqs = get_freqs(pixels, sample_rate, duration_s, x, y, &n);
+    float *freqs = get_freqs(pixels, sample_rate, time_s, x, y, &n);
     free(pixels);
     if (!freqs) return EXIT_FAILURE;
 
     /* Wav files expect amplitudes between [-1.0, 1.0] */
-    clamp(freqs, n);
+    normalize(freqs, n);
 
     wav_write_config cfg = {1, n, sample_rate};
     wav_write(cfg, argv[4], freqs);
